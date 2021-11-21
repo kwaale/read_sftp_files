b8aaa206a0b9cae72d88eb0ce6737f73a8d41a8a
Knut Waale <kwaale@github.com>
2021-11-20 21:31:29 -0300

    g

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const ftp = require("basic-ftp");
+const fs = require("fs");
 
 example();
 
@@ -7,24 +8,34 @@ async function example(){
     client.ftp.verbose = true;
     try {
         console.log('linea 1');
+        // const secureOptions = {
+        //     // Necessary only if the server requires client certificate authentication.
+        //     key: fs.readFileSync('client-key.pem'),
+        //     cert: fs.readFileSync('client-cert.pem'),
+        //     // Necessary only if the server uses a self-signed certificate.
+        //     ca: [ fs.readFileSync('server-cert.pem') ],
+        //     // Necessary only if the server's cert isn't for "localhost".
+        //     checkServerIdentity: () => { return null; },
+        //   };
         await client.access({
-            host:"194.5.159.161",
+            host:"194.005.159.161",
             user:"mguida",
             password:"QWEzxc2021##",
             secure:true,
             port:22,
-        })
+            //secureOptions:secureOptions
+        });
+        await client.ensureDir("/filtros")
         console.log('linea 2');
         
        //Carpeta: /filtros
-       //await client.ensureDir("/filtros/")
-        console.log('await client.list("/filtros")');
-        //console.log(await client.list("/filtros"));
+        console.log(await client.list("/filtros"));
         //await client.uploadFrom("Callcenter_LVC_Envio_Encuesta_20210824_1.csv")
         //await client.downloadTo("Callcenter_LVC_Envio_Encuesta_20210824_1.csv")
     } catch (error) {
         console.log('XXXX',error)
     }
+    console.log('linea 3');
     client.close();
 }
 
